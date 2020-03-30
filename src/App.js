c9bac7257c3166100798bf01f7ad6e95bdbafe01
vasu2912 <vasu.kandagatla@gmail.com>
2020-03-31 00:49:07 +0530

    Added Global data .... and local data remaing

--- src/App.js
+++ src/App.js
@@ -1,26 +1,75 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Axios from "axios";
+import {
+  IonApp,
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonCard,
+  IonCardHeader,
+  IonCardSubtitle,
+  IonCardTitle,
+  IonCardContent,
+  IonItem,
+  IonIcon,
+  IonLabel,
+  IonButton
+} from "@ionic/react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    confirmed: 0,
+    recovered: 0,
+    deaths: 0
+  };
+
+  componentDidMount() {
+    this.getData();
+  }
+
+  async getData() {
+    const res = await Axios.get("https://covid19.mathdro.id/api");
+    this.setState({
+      confirmed: res.data.confirmed.value,
+      recovered: res.data.recovered.value,
+      deaths: res.data.deaths.value
+    });
+  }
+
+  render() {
+    return (
+      <IonApp>
+        <IonHeader>
+          <IonToolbar>
+            <IonTitle>CoVid-19 Tracker</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <IonContent>
+          <ion-card>
+            <ion-card-header>
+              <ion-card-subtitle>Confirmed Cases</ion-card-subtitle>
+              <ion-card-title>{this.state.confirmed}</ion-card-title>
+            </ion-card-header>
+          </ion-card>
+          <ion-card>
+            <ion-card-header>
+              <ion-card-subtitle>Recovered Cases</ion-card-subtitle>
+              <ion-card-title>{this.state.recovered}</ion-card-title>
+            </ion-card-header>
+          </ion-card>
+          <ion-card>
+            <ion-card-header>
+              <ion-card-subtitle>Deaths</ion-card-subtitle>
+              <ion-card-title>{this.state.deaths}</ion-card-title>
+            </ion-card-header>
+          </ion-card>
+        </IonContent>
+      </IonApp>
+    );
+  }
 }
 
 export default App;
