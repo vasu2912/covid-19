231251e278e5a4f302edeea8e4fa71af30ed21ce
vasu2912 <vasu.kandagatla@gmail.com>
2020-04-01 22:39:32 +0530

    loading and footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 import Axios from "axios";
+import { Stretch } from "styled-loaders-react";
 import {
   IonApp,
   IonContent,
@@ -15,19 +16,37 @@ import {
   IonCardContent,
   IonItem,
   IonIcon,
-  IonLabel,
-  IonButton
+  IonLabel
 } from "@ionic/react";
 
 class App extends React.Component {
   state = {
     confirmed: 0,
     recovered: 0,
-    deaths: 0
+    deaths: 0,
+    count: 0,
+    loading: true,
+    date: new Date()
   };
 
   componentDidMount() {
     this.getData();
+    this.country();
+    setTimeout(() => {
+      this.setState({
+        loading: false
+      });
+    }, 3000);
+  }
+
+  async country() {
+    const ccount = await Axios.get(
+      "https://coronavirus-tracker-api.herokuapp.com/v2/locations"
+    );
+    this.setState({
+      count: ccount.data.locations.length - 50,
+      loading: false
+    });
   }
 
   async getData() {
@@ -38,16 +57,31 @@ class App extends React.Component {
       deaths: res.data.deaths.value
     });
   }
-
   render() {
     return (
       <IonApp mode="ios">
         <IonHeader>
-          <IonToolbar>
-            <IonTitle className="title">CoVid-19 Tracker</IonTitle>
+          <IonToolbar color="primary" className="title">
+            <IonTitle className="headtitle">CoVid-19 Tracker</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <IonContent color="dark">
+        <IonContent color="dark" class="ion-padding">
+          <ion-text color="light" className="total">
+            <h1>
+              {this.state.date.toLocaleDateString("nl")}
+              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;at-
+              {this.state.date.toLocaleTimeString()}
+            </h1>
+          </ion-text>
+          <ion-text class="total">
+            {this.state.loading ? (
+              <Stretch color="white" />
+            ) : (
+              <div class="ion-text-center">
+                Total Effected Countries : {this.state.count}
+              </div>
+            )}
+          </ion-text>
           <ion-card>
             <ion-card-header color="warning">
               <ion-card-title>Total Confirmed Cases</ion-card-title>
@@ -67,6 +101,11 @@ class App extends React.Component {
             </ion-card-header>
           </ion-card>
         </IonContent>
+        <ion-footer class="ion-no-border" translucent="true">
+          <ion-toolbar color="dark">
+            <ion-title>github.com/vasu2912</ion-title>
+          </ion-toolbar>
+        </ion-footer>
       </IonApp>
     );
   }
